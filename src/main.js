863fd6eda8b09f5f521ae11dc25b210ec679d12b
Dmitry <dymonius12@gmail.com>
2018-06-07 00:05:43 +0300

    FireBase customize

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,15 @@ new Vue({
   router,
   store,
   components: {App},
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyDWylm9Kc_keiuU6uTYl4zOQyOAbR7qs_s',
+      authDomain: 'itc-ad-project.firebaseapp.com',
+      databaseURL: 'https://itc-ad-project.firebaseio.com',
+      projectId: 'itc-ad-project',
+      storageBucket: 'itc-ad-project.appspot.com',
+      messagingSenderId: '1002032439285'
+    })
+  }
 })
